e053c6b1fdbd798a8f1093b93a9a0896edacf3a8
Ahmad Al-khateeb <Ahmad Al-khateeb@192.168.1.101>
2016-09-10 17:43:38 +0300

    add admin pages

--- Portal/View/AdminWidgets/addItems.php
+++ Portal/View/AdminWidgets/addItems.php
@@ -8,21 +8,21 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title>Admin-Panel</title>
-        <link href="../../Content/css/bootstrap.css" rel='stylesheet' type='text/css' />
+        <link href="../../Contentcss/bootstrap.css" rel='stylesheet' type='text/css' />
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <script src="../../Content/js/jquery.min.js"></script>
+        href="../../Content/js/jquery.min.js"></script>
         <!-- Custom Theme files -->
-        <link href="../../Content/css/style.css" rel='stylesheet' type='text/css' />
+        <link href="../../Contentcss/style.css" rel='stylesheet' type='text/css' />
         <!-- Custom Theme files -->
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
         <!--webfont-->
         <link href='http://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
         <link href='http://fonts.googleapis.com/css?family=Roboto:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
-        <script src="../../Content/js/jquery.easydropdown.js"></script>
+        href="../../Content/js/jquery.easydropdown.js"></script>
         <!-- Add fancyBox main JS and CSS files -->
-        <script src="../../Content/js/jquery.magnific-popup.js" type="text/javascript"></script>
-        <link href="../../Content/css/magnific-popup.css" rel="stylesheet" type="text/css">
+        href="../../Content/js/jquery.magnific-popup.js" type="text/javascript"></script>
+        <link href="../../Contentcss/magnific-popup.css" rel="stylesheet" type="text/css">
     </head>
     <body>
 
